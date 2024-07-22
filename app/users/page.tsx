def02a7d9b60f002dc1e331cb6d8367f86f89b60
Zartab Nakhwa <zartab@codewithz.com>
2024-07-22 17:21:38 +0530

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,9 +9,7 @@ export default async function UsersPage() {
 
     const response=await fetch("https://jsonplaceholder.typicode.com/users",
         {
-            next:{
-                revalidate:10
-            }
+            cache:'no-store'
         }
     )
     const users: User[]=await response.json();
@@ -19,6 +17,7 @@ export default async function UsersPage() {
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
     <ul>
         {
             users.map(
