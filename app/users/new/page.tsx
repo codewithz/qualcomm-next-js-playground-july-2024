47ff658fb07a65f69d0bab54b4589a4a684736da
Zartab Nakhwa <zartab@codewithz.com>
2024-07-23 12:22:22 +0530

    Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function NewUserPage() {
+
+  const router=useRouter()
   return (
     <div>
       <h1>New User Home Page</h1>
+      <button
+      className='btn btn-accent'
+      onClick={()=>router.push("/users")}
+      >
+        Create
+      </button>
     </div>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserTable from './UserTable'
+import Link from 'next/link';
 
 interface Props{
   searchParams:{
@@ -14,6 +15,9 @@ export default function UsersPage({searchParams:{sortOrder}}:Props) {
   return (
     <>
     <h1>Users</h1>
+    <Link href="/users/new" className='btn btn-neutral'>
+    New User
+    </Link>
     <UserTable sortOrder={sortOrder}/>
     </>
   )
