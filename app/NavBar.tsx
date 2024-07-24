0a2937da9cf251614eb30b069201e70d21e7ddfa
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 14:42:46 +0530

    Accessing Session on Client Side

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import { useSession } from 'next-auth/react'
 
 export default function NavBar() {
+
+  const {status,data:session}=useSession()
   return (
     <div className='flex bg-slate-200 p-5 space-x-3'>
         <Link href="/" className='mr-5'>
@@ -11,7 +16,9 @@ export default function NavBar() {
         Users
         </Link>
         <Link href="/admin">Admin</Link>
-        <Link href="/api/auth/signin">Login</Link>
+        {status === 'authenticated' && <div>{session.user!.name}</div> }
+        {status === 'unauthenticated' && 
+        <Link href="/api/auth/signin">Login</Link>}
       
     </div>
   )
