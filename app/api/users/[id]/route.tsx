31c4699054887361d6fcfb5832a7c70c5f276154
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 17:04:46 +0530

    Builds are fixed

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,7 +7,7 @@ export async function GET(
   { params }: { params: { id: string } }) {
 
     const user=await prisma.user.findUnique({
-      where:{id:parseInt(params.id)}
+      where:{id:(params.id)}
     });
 
     if (!user)
@@ -25,7 +25,7 @@ export async function PUT(
     return NextResponse.json(validation.error.errors, { status: 400 })
 
   const user =await prisma.user.findUnique({
-    where:{id:parseInt(params.id)}
+    where:{id:(params.id)}
   })
   if(!user){
         return NextResponse.json({ error: 'User not found'}, { status: 404 })
@@ -47,7 +47,7 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   const user =await prisma.user.findUnique({
-    where:{id:parseInt(params.id)}
+    where:{id:params.id}
   })
   if(!user){
         return NextResponse.json({ error: 'User not found'}, { status: 404 })
