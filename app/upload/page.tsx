0b62e6ea80c55c8a4a5b2b190be7b69565e81888
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 11:56:19 +0530

    Show image with customized upload widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,12 +1,34 @@
 "use client"
 
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary'
+import React, { useState } from 'react'
+import { CldUploadWidget,CldImage } from 'next-cloudinary'
+
+interface CloudinaryResult{
+    public_id:string;
+}
 
 export default function UploadPage() {
+    const [publicId,setPublicId]=useState("");
+
+
   return (
     <div>
-      <CldUploadWidget uploadPreset='cwz_next_app_preset'>
+        {publicId && <CldImage src={publicId} width={270} height={270} alt="an image"/>}
+      <CldUploadWidget 
+      options={
+        {
+            sources:['local'],
+            multiple:false
+        }
+      }
+      uploadPreset='cwz_next_app_preset'
+      onUpload={(result,widget)=>{
+        console.log(result);
+        if(result.event !== "success") return;
+        const info=result.info as CloudinaryResult;
+        setPublicId(info.public_id)
+      }}
+      >
         {({open})=>(
             <button 
             className='btn btn-primary'
