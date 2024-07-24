13a959890abddbdf049ef5da308de9480b9d1bc4
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 14:55:46 +0530

    Accessing Session on Server Side

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import ProductCard from './components/ProductCard'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+
+  const session=await getServerSession(authOptions);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Welcome to Qualcomm Next JS Training</h1>
+        <h1>Welcome &nbsp;
+          {session && <span>{session.user!.name}</span>}</h1>
+          <h3>to Qualcomm Next JS Training</h3>
         <Link href="/users">Users</Link>
         <ProductCard />
       </div>
